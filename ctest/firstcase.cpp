df4fcb7438a6dfc79baed5304d158dee2b1b3ec2
kai.yu2 <kai.yu2@nio.com>
2024-07-17 21:16:30 +0800

    ctest

---
+++ ctest/firstcase.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// 函数声明
+int addNumbers(int a, int b);
+
+int main() {
+    int  c,num1, num2, sum;
+
+    // 获取用户输入
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    // 调用函数计算和
+    sum = addNumbers(num1, num2);
+    
+
+    // 输出结果
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << std::endl;
+     int a = 10, b = 0;
+    std::cout << "Dividing by zero: ";
+    std::cout << a / b << std::endl;  // 错误2：除以零
+
+    return 0;
+}
+
+// 函数定义
+int addNumbers(int a, int b) {
+    
+    return a + b;
+}
